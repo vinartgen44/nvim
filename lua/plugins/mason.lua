0224aa95137bb3b0070d0716d9057f1ee871de54
Artem Vinogradov <vinartgen@outlook.com>
2024-11-29 15:08:39 +0300

    add treesitter,masom and settings color

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,5 +4,14 @@ return {
 		config = function()
 			require("mason").setup({})
 		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				  ensure_installed = { "lua_ls", "rust_analyzer" },
+			})
+		end
+
 	}
 }
